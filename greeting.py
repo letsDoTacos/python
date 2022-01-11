4c456194206526962d11c64b2f7094de7e2a892f
jp <69607974+letsDoTacos@users.noreply.github.com>
2022-01-11 12:51:06 -0600

    completed ps py for beg's

---
+++ greeting.py
@@ -0,0 +1,16 @@
+# def greeting(name):
+#     print('Hello', name)
+
+# # Main program
+# input_name = input("Enter your name:\n")
+
+# greeting(input_name)
+
+def addation(a, b):
+    return a + b
+# Main program 
+num1 = float(input("Enter your 1st num:\n"))
+num2 = float(input('Enter your 2nd num:\n'))
+#Calling our func
+result = addation(num1, num2)
+print('The result is', result)
--- weather.py
+++ weather.py
@@ -1,23 +1,33 @@
 import requests
 
-api_key = "e945998b15f9c864b432b026a1255bc3"
-city = "Orlando"
-url = (
-    "http://api.openweathermap.org/data/2.5/weather?q="
-    + city
-    + "&appid="
-    + api_key
-    + "&units=imperial"
-)
+
+def get_weather_desc_and_temp():
+    api_key = "e945998b15f9c864b432b026a1255bc3"
+    city = "Orlando"
+    url = (
+        "http://api.openweathermap.org/data/2.5/weather?q="
+        + city
+        + "&appid="
+        + api_key
+        + "&units=imperial"
+    )
+
+    request = requests.get(url)
+    json = request.json()
+
+    description = json.get("weather")[0].get("description")
+    temp_min = json.get("main").get("temp_min")
+    temp_max = json.get("main").get("temp_max")
+
+    return {"description": description, "temp_min": temp_min, "temp_max": temp_max}
 
 
-request = requests.get(url)
-json = request.json()
-# print(json)
+def main():
+    weataher_dict = get_weather_desc_and_temp()
+    # print(json)
+    print("Todays forcast is", weataher_dict.get("description"))
+    print("The min temperature is", weataher_dict.get("temp_min"))
+    print("The max temperature is", weataher_dict.get("temp_max"))
 
-description = json.get("weather")[0].get("description")
-print("Todays forcast is", description)
-temp_min = json.get("main").get("temp_min")
-print("The min temperature is", temp_min)
-temp_max = json.get("main").get("temp_max")
-print("The max temperature is", temp_max)
+
+main()
